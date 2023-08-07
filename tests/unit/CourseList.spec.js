e6f3d728b589628d2f52dbaf33e20cd953a877d7
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-08-07 19:21:19 -0400

    One more test added to CourseList

--- tests/unit/CourseList.spec.js
+++ tests/unit/CourseList.spec.js
@@ -6,4 +6,23 @@ describe("CourseList.vue", () => {
     const wrapper = mount(CourseList);
     expect(wrapper.exists()).toBe(true);
   });
+
+  it("emit event when click on course", async () => {
+    const wrapper = mount(CourseList);
+    const course = {
+      name: "Test.js",
+      description: "The Progressive JavaScript Framework",
+      hours: 50,
+      credits: 3,
+      location: "Online",
+      instructor: "John Doe",
+      id: 1,
+      enrollment: 20,
+    };
+
+    await wrapper.vm.$emit("addCourse", course.id);
+    expect(wrapper.emitted("addCourse")[0]).toEqual([course.id]);
+  });
+
 });
+
